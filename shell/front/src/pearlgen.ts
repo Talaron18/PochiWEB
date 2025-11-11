3390a979acd301289f5ebc4e383c18ee83daa289
Talaron18 <mainauctor@gmail.com>
2025-11-11 23:38:14 +0800

    modified:   shell/front/src/pearlgen.ts

--- shell/front/src/pearlgen.ts
+++ shell/front/src/pearlgen.ts
@@ -1,6 +1,23 @@
 import axios from "axios";
 import FormData from "form-data";
 import fs from "fs";
-async function getpearl(){
-    const payload=await 
+async function getpearl(input: Promise<any>){
+    const payload = await input;
+    const response = await axios.postForm(
+        `https://api.stability.ai/v2beta/stable-image/generate/ultra`,
+        axios.toFormData(payload, new FormData()),
+        {
+            validateStatus:undefined,
+            responseType: "arraybuffer",
+            headers:{
+                Authorization: `Bearer sk-APIKEY`,
+                Accept:"image/*"
+            },
+        },
+    );
+    if(response.status === 200) {
+        fs.writeFileSync("./lighthouse.webp", Buffer.from(response.data));
+    } else {
+        throw new Error(`${response.status}: ${response.data.toString()}`);
+    }
 }
